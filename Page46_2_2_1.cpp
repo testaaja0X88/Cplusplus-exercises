0b3266974d049a4ca383cf833e158b00efe1aaf0
testaaja0X88 <datacamp77@suomi24.fi>
2017-06-21 10:26:14 +0300

    Add files via upload
    parannuksia

---
+++ Page46_2_2_1.cpp
@@ -0,0 +1,43 @@
+/*
+Write a program to print all the numbers from n1 to n2, where n1 and n2 are two
+numbers specified by the user. (Hint: You’ll need to prompt for two values n1 and n2;
+then, initialize i to n1 and use n2 in the loop condition.)
+
+*/
+
+#include "stdafx.h"
+#include <iostream> 
+using namespace std;
+
+int main() {
+	int start = 0;
+	int end = 0;
+
+	cout << "Enter a number for \"start\" and press ENTER: ";
+	cin >> start;
+	cout << "Enter a number for \"end\" and press ENTER: ";
+	cin >> end;
+
+
+	if (start <= end)
+	{
+		while (start <= end)
+		{  // While i less than or equal n, 
+			cout << start << " ";   //   Print i, 
+			start += 1;          //   Add 1 to i.    
+		}
+	}
+	else
+	{
+		while (start >= end)
+		{  // While i less than or equal n, 
+			cout << start << " ";   //   Print i, 
+			start -= 1;          //   Add 1 to i.    
+		}
+	}
+
+	cout << endl << endl;
+	return 0;
+}
+
+
